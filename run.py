488b44f8ac6ca1e5ec37bc85395bf98f04a68de6
Anup <149822306+anupddas@users.noreply.github.com>
2025-03-02 15:53:33 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+# Import the createApp function from the app module.
+from app import createApp
 
-app = create_app()
+# Initialize the Flask application instance using the createApp function.
+appInstance = createApp()
 
+# If this script is executed as the main program, run the Flask app in debug mode.
 if __name__ == '__main__':
-    app.run(debug=True)
+    appInstance.run(debug=True)
